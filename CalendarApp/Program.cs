944fd2720a12a9f7c2cdafc1e1d642546c05ee71
user <user@DESKTOP-GAP52DF>
2024-02-29 08:40:47 +0100

    Created boilerplate.

--- CalendarApp/Program.cs
+++ CalendarApp/Program.cs
@@ -1,24 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace CalendarApp
 {
-    static class Program
+    public class CalendarForm : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        private MonthCalendar calendar;
+        private Button updateButton;
+
+        public CalendarForm()
+        {
+            Text = "Kalender App";
+            Size = new System.Drawing.Size(300, 300);
+
+            calendar = new MonthCalendar();
+            calendar.Location = new System.Drawing.Point(10, 10);
+            calendar.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            Controls.Add(calendar);
+
+            updateButton = new Button();
+            updateButton.Text = "neu";
+            updateButton.Location = new System.Drawing.Point(10, 220);
+            updateButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            updateButton.Click += UpdateCalendar;
+            Controls.Add(updateButton);
+        }
+
+        private void UpdateCalendar(object sender, EventArgs e)
+        {
+            // Hier könntest du die Logik implementieren, um den Kalender zu aktualisieren.
+            // In diesem Beispiel wird einfach der Kalender des aktuellen Monats erneut gesetzt.
+            Controls.Remove(calendar);
+            calendar.Dispose();
+
+            calendar = new MonthCalendar();
+            calendar.Location = new System.Drawing.Point(10, 10);
+            calendar.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            Controls.Add(calendar);
+        }
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            // Ändern Sie hier Form1() zu Ihrer neuen Form-Klasse
-            Application.Run(new Form1());
+            Application.Run(new CalendarForm());
         }
     }
 }
