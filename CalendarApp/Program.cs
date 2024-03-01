c211f40939c490296f2ab487c5d4871ac5a47b93
user <user@DESKTOP-GAP52DF>
2024-03-01 12:18:08 +0100

    Boilerplate II

--- CalendarApp/Program.cs
+++ CalendarApp/Program.cs
@@ -12,47 +12,36 @@ namespace CalendarApp
         public CalendarForm()
         {
             Text = "Kalender App";
-            Size = new System.Drawing.Size(300, 300);
+            Size = new System.Drawing.Size(500, 500);
+
+            // Panels erstellen und hinzufügen
+            for (int i = 0; i < 3; i++)
+            {
+                Panel panel = new Panel();
+                panel.BackColor = Color.Blue;
+                panel.Size = new Size(50, 50);
+                panel.Location = new Point(10 + i * 160, 10);
+                Controls.Add(panel);
+            }
 
             calendar = new MonthCalendar();
-            calendar.Location = new System.Drawing.Point(10, 10);
-            calendar.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            calendar.BackColor = System.Drawing.Color.Purple; //TODO: Wieso wird das Ding nicht lila??
-
-            calendar.DateSelected += new System.Windows.Forms.DateRangeEventHandler(Calendar_DateSelected);
+            calendar.Location = new System.Drawing.Point(10, 180);
+            calendar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            calendar.ForeColor = System.Drawing.Color.Purple;
 
             Controls.Add(calendar);
 
             updateButton = new Button();
             updateButton.Text = "neu";
-            updateButton.Location = new System.Drawing.Point(10, 220);
+            updateButton.Location = new System.Drawing.Point(10, 380);
             updateButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             updateButton.Click += UpdateCalendar;
             Controls.Add(updateButton);
         }
 
-        private void Calendar_DateSelected(object sender, DateRangeEventArgs e) {
-            // Show the start and end dates in the text box.
-            Console.WriteLine("Date Selected: Start = " +
-                e.Start.ToShortDateString() + " : End = " + e.End.ToShortDateString());
-        }
-
         private void UpdateCalendar(object sender, EventArgs e)
         {
-            // Show the start and end dates in the text box.
             Console.WriteLine("What the heck am I supposed to do?");
-            // Hier könntest du die Logik implementieren, um den Kalender zu aktualisieren.
-            // In diesem Beispiel wird einfach der Kalender des aktuellen Monats erneut gesetzt.
-
-            /*
-            Controls.Remove(calendar);
-            calendar.Dispose();
-
-            calendar = new MonthCalendar();
-            calendar.Location = new System.Drawing.Point(10, 10);
-            calendar.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            Controls.Add(calendar);
-            */
         }
 
         [STAThread]
